0ad433566a40b8a9038bef6c2b7ebd6b3b1fbe43
Juan Amaya <amayajuan95@gmail.com>
2019-01-18 16:42:45 -0600

    Cotizador

---
+++ src/Controller/CotizadorController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CotizadorController extends AbstractController
+{
+    /**
+     * @Route("/cotizador", name="cotizador")
+     */
+    public function cotizador()
+    {
+        return $this->render('cotizador/cotizador.html.twig', [
+            'controller_name' => 'CotizadorController',
+        ]);
+    }
+}
