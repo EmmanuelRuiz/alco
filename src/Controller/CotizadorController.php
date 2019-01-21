8035ff7ed4dd6a81752218828af30bfa73e3347d
Juan Amaya <amayajuan95@gmail.com>
2019-01-21 16:48:11 -0500

    Empezando con el cotizador

--- src/Controller/CotizadorController.php
+++ src/Controller/CotizadorController.php
@@ -5,13 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\ServiceCategory;
+
 class CotizadorController extends AbstractController
 {
     /**
      * @Route("/cotizador", name="cotizador")
      */
-    public function cotizador()
-    {
-        return $this->render('cotizador/cotizador.html.twig');
+    public function cotizador(){
+
+    	$em = $this->getDoctrine()->getManager();
+
+    	$service_categories_repo = $this->getDoctrine()->getRepository(ServiceCategory::class);
+
+    	$service_categories = $service_categories_repo->findAll();
+
+
+
+		
+
+        return $this->render('cotizador/cotizador.html.twig', [
+        	'service_categories' => $service_categories
+        ]);
     }
 }
