e09727e65c7ffea6b5df451a2b1ce671240f6ea5
Andy <andyrat_96@hotmail.com>
2019-02-27 12:31:06 -0500

    Form

--- src/Controller/CotizadorController.php
+++ src/Controller/CotizadorController.php
@@ -32,45 +32,14 @@ class CotizadorController extends AbstractController
 
         $services = $request->get("services");
 
-        
-
-            $descriptions_repo = $this->getDoctrine()->getRepository(ServiceDescription::class);
-            $descriptions = $descriptions_repo->findBy([
-                'id' => $services
-            ]);
-
-            /*
-            foreach($description as $prices ) {
-                $prices->getPrice();
-            }
-            */
+        $descriptions_repo = $this->getDoctrine()->getRepository(ServiceDescription::class);
+        $descriptions = $descriptions_repo->findBy([
+            'id' => $services
+        ]);
 
         return $this->render('cotizador/precios.html.twig', [
             'descriptions' => $descriptions
         ]);
-
-        /* Estoy recorriendo los provedores para sacar el nombre de cada uno pero en ajax solo recibe el nombre del ultimo, no todos asi ques hay que ver como conseguir todos */
-        /*
-        if (count($providers) >= 1) {
-            foreach ($providers as $provider) {
-                $result = $provider->getProvider();
-            }
-            var_dump($providers);
-        }else{
-            $result = "Sabra que hace";
-        }
-        */
-
-        /*
-        $em = $this->getDoctrine()->getManager();
-        $descriptions_repo = $em->getRepository(ServiceDescription::class);
-        
-        $dql = "SELECT provider FROM service_description WHERE service_category_id = '$category'";
-        $query = $em->createQuery($dql);
-        $result = $query->execute();
-        */
-
-        //return new Response($result);
 
     }
 
